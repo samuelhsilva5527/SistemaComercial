1419033766f37ae3c0cfe5f5b0b1cfe060bb0343
Samuel Henrique da Silva <samuel.henrique@dcx.ufpb.br>
2021-04-18 16:30:34 -0300

    InitialSistemaComercial

---
+++ sitemaComercial/src/test/java/br/ufpb/sistemaComercial/testeTest.java
@@ -0,0 +1,19 @@
+package br.ufpb.sistemaComercial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
